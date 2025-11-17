d7d15e85b2299dfbe998c566347884c0886d72ed
Priyabrata Singh <priyabratasingh2005@gmail.com>
2025-11-17 11:43:19 +0530

    just to see the login page i added the component to the main app

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,12 @@
 import Navbar from '../components/Navbar'
 import LandingPage from '@/components/LandingPage';
+import AuthForm from '@/components/LoginPage';
 export default function Home() {
   return (
     <>
     <Navbar />
     <LandingPage />
+    <AuthForm />
     </>
       );
 }
